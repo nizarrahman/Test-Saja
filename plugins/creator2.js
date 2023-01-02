0e64b9356733753452c8a6934e1eb9f2b2e15d1f
Nizar Rahman <102470420+nizarrahman@users.noreply.github.com>
2023-01-03 04:07:50 +0700

    Update creator2.js

--- plugins/creator2.js
+++ plugins/creator2.js
@@ -6,7 +6,7 @@ let name = await conn.getName(who)
 
 
 if (command == 'whmods1') {
- let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Saya Owner Nai Bot;Bot;;Md\nFN:Saya Owner Bot Nai Store WhatsApp, Md\nNICKNAME:👑 Owner Bot Nai Store\nORG:WH\nTITLE:MODS\nitem1.TEL;waid=6281386945391:+62 813-8694-5391\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://www.nizarr.my.id\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:rnizar221@gmail.com\nitem3.X-ABLabel:💌 Mail Owner WH MODS BOT\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 KEPO LU YAH\nEND:VCARD`
+ let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Saya Owner Nai Bot;Bot;;Md\nFN:Saya Owner Bot Nai Store WhatsApp, Md\nNICKNAME:👑 Owner Bot Nai Store\nORG:WH\nTITLE:MODS\nitem1.TEL;waid=6281386945391:+62 813-8694-5391\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://www.nizarr.my.id\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:rnizar221@gmail.com\nitem3.X-ABLabel:💌 Mail Owner BOT Nai Store\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 KEPO LU YAH\nEND:VCARD`
 const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fakes })
 let caption = `👋 Hai *${name} @${who.split("@")[0]}*, Nih Owner *${conn.user.name}* kak`
     await conn.sendButton(m.chat, caption, author, null, [['🎀 Sapa Owner', 'Huuu']], m, { quoted: tag_own, mentions: conn.parseMention(caption) })
